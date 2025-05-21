ae464d539dee4b70669602ae76224d4b88ed6d45
Odonata-Masoni <investgramvn@gmail.com>
2025-05-21 12:20:22 +0700

    Them movement
    Da cho character di chuyen duoc

---
+++ Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player_Movement : MonoBehaviour
+{
+    //Goi tham so
+    public float moveSpeed = 5f;
+
+    //Kiem tra trang thai
+    public bool isGrounded = true;
+
+    //Components
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+    private void FixedUpdate()
+    {
+        rb.velocity = moveInput * moveSpeed;
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+}
